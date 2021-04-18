80326dd52a6493f1204b30550a41a5faf683f39c
Ryan Flett <ryan.flett@hotmail.com>
2021-04-18 19:09:16 +1000

    Add get/set to user profile

--- lib/view/pages/logged_in/profile/sub_pages/my_profile_page.dart
+++ lib/view/pages/logged_in/profile/sub_pages/my_profile_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:jui/models/dto/request/user/update_user.dart';
+import 'package:jui/models/dto/response/problem_response.dart';
+import 'package:jui/models/dto/response/user/user.dart';
+import 'package:jui/server/user.dart';
+import 'package:jui/utilities/popups.dart';
+import 'package:jui/utilities/token.dart';
 import 'package:jui/view/pages/logged_in/components/user_avatar.dart';
 
 class MyProfilePage extends StatefulWidget {
@@ -9,9 +15,40 @@ class MyProfilePage extends StatefulWidget {
 }
 
 class _MyProfilePageState extends State<MyProfilePage> {
+  UserResponse? _user;
   String _nickname = "";
 
-  void _onUpdateClicked() {}
+  @override
+  void initState() {
+    super.initState();
+    this._getProfileData();
+  }
+
+  _getProfileData() async {
+    try {
+      // retrieve the user id from the stored token
+      var tkn = await Token.get();
+      var user = await User.get(tkn.sub, withVotes: false);
+      // set the vars
+      this._user = user;
+      this._nickname = user.nickName == null ? "" : user.nickName!;
+    } catch (err) {
+      // TODO logging
+      print(err);
+      PopupUtils.showError(context, err as ProblemResponse);
+    }
+  }
+
+  void _onUpdateClicked() async {
+    var requestData = UpdateUserRequest(this._nickname);
+    try {
+      await User.update(requestData);
+    } catch (err) {
+      // TODO logging
+      print(err);
+      PopupUtils.showError(context, err as ProblemResponse);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,12 +60,12 @@ class _MyProfilePageState extends State<MyProfilePage> {
         child: Column(
           children: [
             UserAvatar(
-              uuid: "ffdsf",
+              uuid: _user!.userID,
               size: 200,
             ),
             SizedBox(height: 20),
             Text(
-              "James Turner",
+              _user!.name,
               style: TextStyle(
                 fontSize: 25,
               ),
